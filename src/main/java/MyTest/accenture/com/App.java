be478899f1d591dc4edaf8ccf2a58b3c32b0c1ed
Training_H2A.03.20 <Training_h2a.03.20@accenture.com>
2019-02-12 15:25:25 +0530

    check-in

--- src/main/java/MyTest/accenture/com/App.java
+++ src/main/java/MyTest/accenture/com/App.java
@@ -9,7 +9,8 @@ public class App
 	int a;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World !");
+    	int a;
+System.out.println( "Hello World !");
     }
 }
 	
